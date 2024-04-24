71bcb9992f58c51554abc229e7b93f93a5e336d5
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-04-24 07:38:21 +0600

    array of objects for service SearchBar

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+let serviceList = [
+    {
+        name: "PC workspace maintenance",
+        service: "IT Service"
+    },
+    {
+        name: "Landing web pages",
+        service: "IT Service"
+    },
+    {
+        name: "Printer maintenance",
+        service: "IT Service"
+    },
+    {
+        name: "Hardware repair",
+        service: "IT Service"
+    },
+    {
+        name: "Software maintenance",
+        service: "IT Service"
+    },
+    {
+        name: "Branding Design",
+        service: "Design Service"
+    },
+    {
+        name: "Social Media Creative",
+        service: "Design Service"
+    },
+    {
+        name: "Illustration Design",
+        service: "Design Service"
+    },
+    {
+        name: "Presentation Design",
+        service: "Design Service"
+    },
+    {
+        name: "Logo Design",
+        service: "Design Service"
+    },
+    {
+        name: "Product Design",
+        service: "Design Service"
+    },
+    {
+        name: "Motion Design",
+        service: "Design Service"
+    },
+    {
+        name: "Web Design",
+        service: "Design Service"
+    },
+    {
+        name: "Event Design",
+        service: "Design Service"
+    },
+    {
+        name: "T-Shirt Design",
+        service: "Design Service"
+    },
+    {
+        name: "Photo Editing",
+        service: "Design Service"
+    },
+    {
+        name: "Social Media Kit Design",
+        service: "Design Service"
+    },
+    {
+        name: "Flyer Design",
+        service: "Design Service"
+    },
+    {
+        name: "Catalogue Design",
+        service: "Design Service"
+    },
+    {
+        name: "UI UX Design",
+        service: "Design Service"
+    },
+    {
+        name: "Label Design",
+        service: "Design Service"
+    },
+    {
+        name: "ID Card Design",
+        service: "Design Service"
+    },
+    {
+        name: "Leaflet Design",
+        service: "Design Service"
+    },
+    {
+        name: "Business Card Design",
+        service: "Design Service"
+    },
+    
+    {
+        name: "Ai fitness trainer",
+        service: "Ai Services"
+    },
+    {
+        name: "Ai health care",
+        service: "Ai Services"
+    },
+    {
+        name: "Ai office manager",
+        service: "Ai Services"
+    },
+    {
+        name: "Ai security",
+        service: "Ai Services"
+    },
+]
+
+const SearchBar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SearchBar;
